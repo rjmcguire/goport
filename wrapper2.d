66e3fa1895e0a8326659a78b19ea1d1a3d4bbf1c
Rory McGuire <rjmcguire@gmail.com>
2013-09-11 16:20:11 +0200

    Weird mixin template classes

--- wrapper2.d
+++ wrapper2.d
@@ -3,7 +3,7 @@ module wrapper2;
 
 import deimos.ev;
 import channel : chan, makeChan, select;
-import goroutine : go;
+import goroutine : go, shutdown, yield;
 
 	import std.stdio;
 
@@ -23,29 +23,60 @@ class Loop {
 	}
 	void run() {
 		while (!input.empty) {
+			writeln("add");
 			input._.add(loop);
+			writeln("added");
 		}
+		writeln("go");
 		ev_run(loop, EVRUN_ONCE);
+			writeln("done");
 	}
 }
-
 class EVReq {
+	enum Type { Idle, Timer }
 	abstract void add(ev_loop_t* loop);
 }
-class EVReqIdle : EVReq {
-	ev_idle watcher;
+class EVReqT(EVReq.Type T) : EVReq {
 	shared chan!bool ready = makeChan!bool(1);
+	mixin WATCHER!(T, typeof(this));
+
 	override
 	void add(ev_loop_t* loop) {
 		watcher.data = cast(void*)this;
-		ev_idle_init(&watcher, &cb);
-		ev_idle_start(loop, &watcher);
+		static if (T==EVReq.Type.Idle) {
+			ev_idle_init(&watcher, &cb);
+			ev_idle_start(loop, &watcher);
+		} static if (T==EVReq.Type.Timer) {
+			ev_timer_init(&watcher, &cb, time, repeat_interval);
+			ev_timer_start(loop, &watcher);
+		} else {
+			assert(0, "unknown evreq type");
+		}
 	}
-	extern(C) static void cb(ev_loop_t* loop, ev_idle *idle, int revents) {
-		EVReqIdle self = cast(EVReqIdle)idle.data;
-		assert(idle is &self.watcher);
-		self.ready._ = true;
-		writeln("okay");
+}
+mixin template WATCHER(EVReq.Type T, SelfType) {
+	static if (T==EVReq.Type.Idle) {
+		ev_idle watcher;
+		extern(C) static void cb(ev_loop_t* loop, ev_idle *idle, int revents) {
+			SelfType self = cast(SelfType)idle.data;
+			assert(idle is &self.watcher);
+			self.ready._ = true;
+			writeln("okay");
+		}
+	} else static if (T==EVReq.Type.Timer) {
+		double time, repeat_interval;
+		this(double time, double repeat_interval) {
+			this.time = time;
+			this.repeat_interval = repeat_interval;
+		}
+		ev_timer watcher;
+		extern(C) static void cb(ev_loop_t* loop, ev_timer* timer, int revents) {
+			SelfType self = cast(SelfType)timer.data;
+			assert(timer is &self.watcher);
+			writeln("timer will okay");
+			self.ready._ = true;
+			writeln("timer okay");
+		}
 	}
 }
 
@@ -53,13 +84,16 @@ class EVReqIdle : EVReq {
 unittest {
 	import std.stdio;
 	auto l = new Loop();
-	auto idler = new EVReqIdle();
+	/*auto idler = new EVReqT!(EVReq.Type.Idle)();
 	l.input._ = idler;
-	auto idler2 = new EVReqIdle();
-	l.input._ = idler2;
-	go!({writeln("run"); l.run(); });
+	auto idler2 = new EVReqT!(EVReq.Type.Idle)();
+	l.input._ = idler2;*/
+	auto timer = new EVReqT!(EVReq.Type.Timer)(10, 0);
+	l.input._ = timer;
 
-	final switch(select(idler.ready, idler2.ready)) {
+
+	go!({for (;;) {writeln("run"); l.run(); yield(); } });
+	final switch(select(timer.ready)) {//idler.ready, idler2.ready)) {
 		case 0:
 			writeln("woot");
 			break;
@@ -67,4 +101,5 @@ unittest {
 			writeln("wart");
 			break;
 	}
+	shutdown();
 }
